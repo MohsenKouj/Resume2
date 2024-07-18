a2241bbaead1e45285d1a84d2b0459c58bd05783
MohsenKouj <misionorido@gmail.com>
2024-07-18 19:16:31 +0430

    late

--- resume/accounts/urls.py
+++ resume/accounts/urls.py
@@ -21,7 +21,7 @@ from . import views
 
 urlpatterns = [
     path('login/',views.login,name='login'),
-    #path('signup/',views.signup,name='signup'),
+    path('signup/',views.signup,name='signup'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- resume/accounts/views.py
+++ resume/accounts/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login(req):
     return render(req, 'pages/login.html')
+
+def signup(req):
+    return render(req, 'pages/signup.html')
