e52d3be911b38d2d741c62861bdc192592054bed
MohsenKouj <misionorido@gmail.com>
2024-07-20 19:21:17 +0430

    late

--- resume/accounts/views.py
+++ resume/accounts/views.py
@@ -1,26 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import authenticate,login as log
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib import messages
 from .forms import *
 # Create your views here.
 def login(req):
-    if req.GET:
-        form = AuthenticationForm()
-    if req.POST:  
-        form = AuthenticationForm(request=req,data=req.POST)                                                                  
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
+    if req.POST:                                                                 
+        username = req.POST.get('username')
+        password = req.POST.get('password')
         user = authenticate(request=req,username=username, password=password) 
         if user is not None:                              
             log(request=req,user=user)                                              
             messages.add_message(req,messages.SUCCESS,"خوش آمدید")                 
-            return HttpResponseRedirect('')                                    
+            return HttpResponseRedirect('/')                                    
         else:
             messages.add_message(req,messages.ERROR,"مشخصات ورودی اشتباه است")                              
         
-    return render(req,'pages/login.html',{'form':form})
+    return render(req,'pages/login.html',)
 
 
 def signup(req):
@@ -31,6 +28,7 @@ def signup(req):
         if cap.is_valid():
             username = req.POST.get('username')
             password = req.POST.get('password')
+            form = UserCreationForm(req.POST)
             return HttpResponseRedirect('/')
         else:
             msg = "<h1>Invalid POST request</h1>"
