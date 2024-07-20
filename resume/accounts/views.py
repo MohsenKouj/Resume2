40c07af0105c0dce00c971e8e958a2078fe59a20
MohsenKouj <misionorido@gmail.com>
2024-07-20 18:41:29 +0430

    late

--- resume/accounts/views.py
+++ resume/accounts/views.py
@@ -1,21 +1,26 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login as log
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from .forms import *
 # Create your views here.
 def login(req):
-    if req.POST:
-        username = req.POST.get('username')
-        password = req.POST.get('password')
-        user = authenticate(request=req,username=username, password=password)
-        if user is not None:
-            messages.add_message(req,messages.SUCCESS,"خوش آمدید")
-            return HttpResponseRedirect('/')
+    if req.GET:
+        form = AuthenticationForm()
+    if req.POST:  
+        form = AuthenticationForm(request=req,data=req.POST)                                                                  
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request=req,username=username, password=password) 
+        if user is not None:                              
+            log(request=req,user=user)                                              
+            messages.add_message(req,messages.SUCCESS,"خوش آمدید")                 
+            return HttpResponseRedirect('')                                    
         else:
-            messages.add_message(req,messages.ERROR,"مشخصات ورودی اشتباه است")
+            messages.add_message(req,messages.ERROR,"مشخصات ورودی اشتباه است")                              
         
-    return render(req, 'pages/login.html')
+    return render(req,'pages/login.html',{'form':form})
 
 
 def signup(req):
